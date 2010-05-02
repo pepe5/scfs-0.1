729c570c45207fd4836bc43885a40ea0abd4fdf3
Josef Kral (@aspire1) <pepe5@email.cz>
2010-05-02 21:31:09 +0200

    add: start

--- lib/test/scfs-fun.rb
+++ lib/test/scfs-fun.rb
@@ -14,18 +14,31 @@ test_setup
 class TestSet1;
   @@id = '$Id$'
   # test aScfs#init
-  def tc1_init;
-    puts "tc1_init: starting at: #{Time .new .strftime "%Y-%m-%d %H:%M:%S"}" # #{@@id}
-    puts "tc1_init: run in:#{$test_site_path}; (started in:#{$start_pwd})"
+  def init_tc1 a=[];
+    puts "init_tc1: starting at: #{Time .new .strftime "%Y-%m-%d %H:%M:%S"}; with: #{a.inspect}" # #{@@id}
+    puts "init_tc1: run in:#{$test_site_path}; (started in:#{$start_pwd})"
     #>! assert prepared (test) site
-    puts `tree -fi` .grep /^.+$/
+    puts `tree -afi` .grep /^.+$/
     puts `ruby #{$start_pwd}/scfs.rb init`
     #>! assert seted up site's .scfs cache stru
     cache_dir = '../.scfs'
-    puts "tc1_init: we got to cache (#{Dir.pwd + cache_dir}):" #>! load paths ~ this cache_dir from env.rb
-    puts `tree -fi #{cache_dir}` .grep /^.+$/ # $(pwd)/
+    puts "init_tc1: we got to cache (#{Dir.pwd + cache_dir}):" #>! load paths ~ this cache_dir from env.rb
+    puts `tree -afi #{cache_dir}` .grep /^.+$/ # $(pwd)/
+    end
+
+  # test aScfs#init
+  def add_tc1 a=[];
+    puts "add_tc1: starting at: #{Time .new .strftime "%Y-%m-%d %H:%M:%S"} with: #{a.inspect}"
+    cache_dir = '../.scfs'
+    #>! assert prepared cache
+    puts `tree -afi #{cache_dir}` .grep /^.+$/
+    puts `ruby #{$start_pwd}/scfs.rb add #{Dir .glob '*'}`
+    #>! assert seted up site's .scfs cache stru
+    puts "add_tc1: we got to cache (#{Dir.pwd + cache_dir}):"
+    puts `tree -afi #{cache_dir}` .grep /^.+$/
     end
 end
 
-if ARGV.size==0; TestSet1.new.tc1_init end
+if ARGV.size==0; TestSet1.new.init_tc1
+else TestSet1.new.send ARGV.shift.to_sym, ARGV end
 puts
--- scfs.rb
+++ scfs.rb
@@ -14,12 +14,20 @@ class Sc;
   # ~ git init - creates cat. tree stru
   def init a=[];
     puts "init: #{a.inspect}.."
-    FileUtils .mkdir_p $config.cache_dir+'/atree', :verbose=>true #, :noop=>true # actual tree (ala top-prrt custom arch)
-    FileUtils .mkdir_p $config.cache_dir+'/fvers', :verbose=>true #, :noop=>true
-    FileUtils .mkdir_p $config.cache_dir+'/vfiles', :verbose=>true #, :noop=>true
-    FileUtils .mkdir_p $config.cache_dir+'/archs', :verbose=>true #, :noop=>true
+    FileUtils .mkdir_p $config.cache_dir+'/.wd', :verbose=>true # actual cwd tree cache~ 'index' (at each $ cd; rebuilt)
+    FileUtils .mkdir_p $config.cache_dir+'/atree', :verbose=>true # actual tree~ index (ala top-prrt custom arch)
+    FileUtils .mkdir_p $config.cache_dir+'/names', :verbose=>true # index by names
+    FileUtils .mkdir_p $config.cache_dir+'/paths', :verbose=>true # index by reverse paths
+    FileUtils .mkdir_p $config.cache_dir+'/=arxs', :verbose=>true # archives' catalog
+    FileUtils .mkdir_p $config.cache_dir+'/=vfiles', :verbose=>true # files' catalog
+    #>! more indexes ~> by time, fvers-count, ..
+    end
+
+  def add a=[];
+    puts "add: #{a.inspect}.."
+    for f in a; puts " <+ #{f}" end
     end
 end
 
-puts "scfs.rb" # ".. p1,rest:(#{[p1, rest] .inspect})"
+puts "scfs.rb: $ scfs #{ARGV .join ' '}" # ".. p1,rest:(#{[p1, rest] .inspect})"
 Sc.new.send ARGV.shift.to_sym, ARGV
