599b2eb541fe7f37adb608085a21a282b6211a98
PB <blani@PB-A8He.(none)>
2012-08-01 21:45:57 +0200

    xattr start

---
+++ lib/xattr-fill.py
@@ -0,0 +1,11 @@
+import os
+import sys
+newin = os.fdopen(sys.stdin.fileno(), 'r', 1)
+
+import sys
+def main ():
+    for line in enumerate (newin):
+        print line
+
+if __name__=='__main__':
+    sys.exit (main ())
