4299a049da667e60bafb418d0318876f095aefc3
PB <blani@PB-A8He.(none)>
2012-08-08 19:42:23 +0200

    $> (cd /tmp/dev/scwd/; ls -1 a) | (python -u lib/xattr-fill.py)

--- lib/xattr-fill.py
+++ lib/xattr-fill.py
@@ -1,8 +1,8 @@
+# test:$> (cd /tmp/dev/scwd/; ls -1 a) | (python -u lib/xattr-fill.py)
+
 import os
 import sys
 from pysqlite2 import dbapi2 as sqlite
-
-filename = 'a'
 
 dbfile = os.path.expanduser ('~/.scfs/cat1.sqlite')
 if not os.path.exists (dbfile):
@@ -11,20 +11,18 @@ con = sqlite.connect (dbfile)
 cur = con.cursor ()
 
 cdlabel = 'WD_UC2'
-cur.execute \
-    ("select * from %s where fileName = '%s'" %
-     (cdlabel+'_files', filename))
 
-rows = cur.fetchall()
-print 'attr/s of %s: %s' % (filename,rows)
+while 1:
+    try:
+        filename = sys.stdin.readline () .rstrip ()
+    except KeyboardInterrupt:
+        break
 
-# while 1: 
-#    try:
-#         line = sys.stdin.readline ()
-#     except KeyboardInterrupt:
-#         break
+    if not filename:
+        break
 
-#     if not line:
-#         break
-
-#     print line
+    cur.execute \
+        ("select * from %s where fileName = '%s'" %
+         (cdlabel+'_files', filename))
+    rows = cur.fetchall()
+    print 'attr/s of %s: %s' % (filename,rows)
