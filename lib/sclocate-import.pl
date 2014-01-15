c563d08bdae3a25500492f94e188fd9f5d3f960f
Josef Kral <kraljo@oc0653706537.ibm.com>
2014-01-15 14:48:16 +0100

    starting w/ find-printf schema

--- lib/sclocate-import.pl
+++ lib/sclocate-import.pl
@@ -1,23 +1,40 @@
 #!/usr/bin/perl -nl
 # read file:~/text/scfs/doc/README.org
-#>! use as: find . -type f -ls | perl -nl $ARGV[0] > $F.import &
+#>! use as: find . -type f -printf "%i %k %M %n %u %g %s %TY-%Tm-%Td %TH:%TM:%TS %h/%f\n" | perl -nl $0 | pv | sqlite3 $F.sqlite
 use POSIX;
-
 BEGIN
 { print "PRAGMA foreign_keys=OFF;
       BEGIN TRANSACTION;
-      CREATE VIRTUAL TABLE find_ls USING fts4
+      CREATE TABLE find_printf
       ( ix INTEGER PRIMARY KEY AUTOINCREMENT,
         dt DATETIME DEFAULT CURRENT_TIMESTAMP,
-        fts4 TEXT
-      );"
-}
+        inode INTEGER,
+        du INTEGER,
+        perms TEXT,
+        lns INTEGER,
+        ug TEXT,
+        s INTEGER,
+        mtime DATETIME,
+        dir TEXT,
+        fn TEXT);" }
 
-s/'/''/g; 
-s/\\([^\\])/$1/g;
-print "INSERT INTO find_ls VALUES
+s/'/''/g;
+m{^
+    (\d+)\s+		# $1 - iNode
+    (\d+)\s+		# $2 - Occupation (kB)
+    (\S+)\s+		# $3 - Permissions (([\w-.+@]+) is not enough for AIX)
+    (\d+)\s+		# $4 - Links #
+    (\w+\s+\w+)\s+	# $5 - User + Group
+    (\d+)\s+		# $6 - Size (B)
+    ([\d-]+\s+[\d:]+)\.\d+\s+ # $7 - Date + Time (%Y-%m-%d %H:%M:%S.<F>)
+    (.*?)		        # $8 - DirName
+    ([^/]+)		        # $9 - BaseName
+    $}x;
+
+# s/\\([^\\])/$1/g; #< deprecated for find-printf
+print "INSERT INTO find_printf VALUES
       ( NULL,
         '".(strftime "%F %T", localtime)."',
-        '$_');";
+        '$1','$2','$3','$4','$5','$6','$7','$8','$9');";
 
 END { print "COMMIT;" }
